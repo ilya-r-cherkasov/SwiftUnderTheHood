3fa450ec384765c8139fe2971c273b5a10e1324e
Ilya Cherkasov <cherkasov-i@surfstudio.ru>
2022-01-15 23:07:49 +0300

    Added empty iOS project

---
+++ SwiftUnderTheHood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftUnderTheHood
+//
+//  Created by Ilya Cherkasov on 15.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
